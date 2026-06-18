90c3b87ad72bd4be3d7e45ce7ab53e08b084fa93
Yash Sinha <aryash3023@outlook.com>
2026-06-18 17:41:25 +0530

    Added Available StockSummary Feature

--- src/main/java/com/cupflow/CupFlow_ERP/inventory/Repository/StockLedgerRepository.java
+++ src/main/java/com/cupflow/CupFlow_ERP/inventory/Repository/StockLedgerRepository.java
@@ -1,6 +1,7 @@
 package com.cupflow.CupFlow_ERP.inventory.Repository;
 
 import com.cupflow.CupFlow_ERP.inventory.EnumsEntity.StockLedger;
+import com.cupflow.CupFlow_ERP.inventory.DTOs.StockSummaryProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,6 +12,18 @@ import java.util.UUID;
 
 public interface StockLedgerRepository extends JpaRepository<StockLedger, UUID> {
 
+    @Query(value = """
+        SELECT  m.id AS materialId,
+                m.material_type AS material_type,
+                m.unit AS unit,
+                m.min_threshold AS minThreshold,
+                COALESCE(SUM(sl.quantity),0) AS availableStock
+        FROM materials m
+        LEFT JOIN stock_ledger sl ON sl.material_id = m.id
+        AND sl.movement_type IN('STOCK_IN','RESERVED')
+        GROUP BY m.id, m.material_type, m.unit, m.min_threshold
+""", nativeQuery = true)
+    List<StockSummaryProjection> getStockSummary();
 
     @Query("""
         SELECT COALESCE(SUM(sl.quantity),0)
